9fd4f77b9788b72c1c0eb4df54585067fd2b592d
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-13 00:38:33 +0000

    linting

--- timer1.js
+++ timer1.js
@@ -1,16 +1,15 @@
 //Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
 
-let arr = process.argv.slice(2)
-console.log(arr)
-  
+let arr = process.argv.slice(2);
+//console.log(arr)
+
 for (const element of arr) {
-  let delay = element * 1000
+  let delay = element * 1000;
   setTimeout(() => {
     //process.stdout.write('\x07');
-    process.stdout.write('BEEP');
-  }, delay)
-  setTimeout(() => { 
+    process.stdout.write("BEEP");
+  }, delay);
+  setTimeout(() => {
     process.stdout.write("\n");
-  }, delay += 10);
-
+  }, (delay += 1));
 }
