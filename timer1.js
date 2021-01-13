83afe590576149fd8669c7559bad6bca402150f9
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-13 00:33:45 +0000

    timer which will beep after a specified amount of time has passed - using setTimeout

---
+++ timer1.js
@@ -0,0 +1,16 @@
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+
+let arr = process.argv.slice(2)
+console.log(arr)
+  
+for (const element of arr) {
+  let delay = element * 1000
+  setTimeout(() => {
+    //process.stdout.write('\x07');
+    process.stdout.write('BEEP');
+  }, delay)
+  setTimeout(() => { 
+    process.stdout.write("\n");
+  }, delay += 10);
+
+}
